93ed3710311fb512be156ef80244ddc5ee6a5e88
johnkagga <johnkagga@gmail.com>
2018-10-29 11:39:59 +0300

    Add delete start code

---
+++ chapter 4/Delete-Starter-code/src/components/App.js
@@ -0,0 +1,159 @@
+import React, {Component} from 'react';
+import image from '../img/vumbulaitem.png';
+import {ItemCard} from "./ItemCard";
+import {Footer} from "./Footer";
+import {Nav} from "./Nav";
+import {Jumbtron} from "./Jumbtron";
+import {AddItem} from "./AddItem";
+
+class App extends Component {
+  state = {
+    name: "",
+    price: "",
+    items: [
+      {
+        id: 1,
+        name: "Noodles",
+        price: "15"
+      },
+      {
+        id: 2,
+        name: "Mangoes",
+        price: "10"
+      },
+      {
+        id: 3,
+        name: "Oranges",
+        price: "20"
+      },
+      {
+        id: 4,
+        name: "Passion Fruits",
+        price: "14"
+      }
+    ]
+  };
+
+  /**
+   * Handle input changes in the AddItem component.
+   * @param event
+   */
+  handleInputChange = event => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  /**
+   * Add an item to state.
+   * @param event
+   */
+  addItem = event => {
+    event.preventDefault();
+    const {name, price} = this.state;
+    const itemsInState = this.state.items;
+    const itemsArrayLength = itemsInState.length;
+    const id = itemsArrayLength ? (itemsInState[itemsArrayLength - 1].id + 1) : 1;
+    this.setState({
+      items: [
+        ...itemsInState,
+        Object.assign({}, {
+          id,
+          name,
+          price
+        })
+      ],
+      name: "",
+      price: ""
+    })
+  };
+
+  /**
+   * Toggle the isEditing property of an item when the Edit button
+   * within ItemCard is clicked.
+   * @param index
+   */
+  toggleItemEditing = index => {
+    this.setState({
+      items: this.state.items.map((item, itemIndex) => {
+        if (itemIndex === index) {
+          return {
+            ...item,
+            isEditing: !item.isEditing
+          }
+        }
+        return item;
+      })
+    });
+  };
+
+  /**
+   * Update the Name and/or Price of an item.
+   * @param event
+   * @param index
+   */
+  handleItemUpdate = (event, index) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      items: this.state.items.map((item, itemIndex) => {
+        if (itemIndex === index) {
+          return {
+            ...item,
+            [name]: value
+          }
+        }
+        return item;
+      })
+    });
+  };
+
+  //TODO 1: Define an arrow function with onDelete as its name and having index as its only argument.
+  //TODO 2: Write out the implementation of the onDelete function.
+  //TODO 3: Define an onDelete prop on the ItemCard component with a value being an arrow function that calls
+  // TODO: the above onDelete function passing it the index of the looped through item
+  render() {
+    const {name, price} = this.state;
+    return <div>
+      <Nav/>
+
+      <Jumbtron/>
+
+      <div className="container pt-4">
+
+        <AddItem
+            name={name}
+            price={price}
+            onChange={this.handleInputChange}
+            onSubmit={this.addItem}
+        />
+
+        <h1 className="display-4 my-4 text-center text-muted">Items</h1>
+
+        <div className="row">
+          {
+            this.state.items.map((item, index) =>
+                <ItemCard
+                    key={item.id}
+                    index={index}
+                    image={image}
+                    item={item}
+                    toggleEditing={() => this.toggleItemEditing(index)}
+                    onChange={this.handleItemUpdate}
+                />
+            )
+          }
+        </div>
+
+        <hr/>
+        <Footer/>
+      </div>
+    </div>;
+  }
+}
+
+export default App;
