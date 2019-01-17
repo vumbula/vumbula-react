01f7f96ae7252ec58cf305e3519d674482f2cc3c
johnkagga <johnkagga@gmail.com>
2019-01-17 12:47:07 +0300

    Add proctected routes example

---
+++ chapter5/Protected-Routes/src/components/Login.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Redirect} from 'react-router-dom';
+
+class Login extends React.Component {
+
+  state = {
+    redirectToReferrer: false
+  };
+
+  login = () => {
+    fakeAuth.authenticate(() => {
+      this.setState({redirectToReferrer: true})
+    })
+  };
+
+  render() {
+    const {from} = this.props.location.state || {from: {pathname: '/'}};
+    const {redirectToReferrer} = this.state;
+
+    if (redirectToReferrer) {
+      return (
+          <Redirect to={from}/>
+      )
+    }
+
+    return (
+        <div>
+          <p>You must log in to view the content at {from.pathname}</p>
+          <button onClick={this.login}>Log in</button>
+        </div>
+    )
+  }
+}
+
+/* A fake authentication function */
+export const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true;
+    setTimeout(cb, 100)
+  },
+};
+
+export default Login
---
+++ chapter5/Protected-Routes/src/components/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Route, Redirect} from "react-router-dom";
+
+export const PrivateRoute = ({component: Component, isAuthenticated, ...rest}) => (
+    <Route {...rest} render={props => (
+        isAuthenticated
+            ? (
+                <Component {...props}/>
+            )
+            :
+            (
+                <Redirect to={{
+                  pathname: '/login',
+                  state: {from: props.location}
+                }}/>
+            )
+    )}/>
+);
